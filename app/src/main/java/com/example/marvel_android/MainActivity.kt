67c162f201c9e4e0e855b550fdec411b84b1e7e6
Letheann <bmadseason@hotmail.com>
2021-07-01 02:00:18 -0300

    code review

--- app/src/main/java/com/example/marvel_android/MainActivity.kt
+++ app/src/main/java/com/example/marvel_android/MainActivity.kt
@@ -7,9 +7,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
